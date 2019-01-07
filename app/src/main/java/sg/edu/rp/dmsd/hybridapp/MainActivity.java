8431a957b1cd7a47118348f18d15239f55d3c99b
17010457 <17010457@myrp.edu.sg>
2019-01-07 21:00:49 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/dmsd/hybridapp/MainActivity.java
@@ -0,0 +1,37 @@
+package sg.edu.rp.dmsd.hybridapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView myWebView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        myWebView = findViewById(R.id.webview1);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setLoadWithOverviewMode(true);
+        webSettings.setUseWideViewPort(true);
+
+        myWebView.loadUrl("http://homes.soi.rp.edu.sg/17010457/c308_p08_snowflake/index.html");
+
+        myWebView.setWebViewClient(new WebViewClient());
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if (myWebView.canGoBack())
+            myWebView.goBack();
+        else
+            super.onBackPressed();
+    }
+}
